4dce4adf20d72820c8b743c9278c2a53eb00c81b
Elle Jacobs <enj657@gmail.com>
2022-01-13 20:30:51 -0600

    Adding joined_df ipynb and csv files

--- heroku_deployment/spotify_hits_app/app.py
+++ heroku_deployment/spotify_hits_app/app.py
@@ -1,4 +1,5 @@
 # Import necessary libraries
+import pandas as pd
 import os
 from flask import (
     Flask,
@@ -12,19 +13,49 @@ app = Flask(__name__)
 
 # Database Setup
 from flask_sqlalchemy import SQLAlchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///spotify.sqlite"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///spotify.sqlite"
 
 # # Remove tracking modifications
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-# from .models import popular_hit
+engine = db.create_engine(app.config['SQLALCHEMY_DATABASE_URI'], {})
 
 # Create route that renders index.html template
 @app.route("/")
 def home():
-    return render_template('index.html', text="Final Project")
+    # with engine.connect() as conn:
+    #     df = pd.read_sql("SELECT * FROM songs", conn)
+    #     html = df.to_html()
+    # return html
+    return render_template('index.html', feature={})
+
+
+# Query the database and send the jsonified results
+@app.route("/send", methods=["GET", "POST"])
+def send():
+    
+    feature = {}
+    feature["genre"] = request.form.get("genre")
+    feature["acousticness"] = request.form.get("acousticness")
+    feature["danceability"] = request.form.get("danceability")
+    feature["energy"] = request.form.get("energy")
+    feature["instrumentalness"] = request.form.get("instrumentalness")
+    feature["liveness"] = request.form.get("liveness")
+    feature["loudness"] = request.form.get("loudness")
+    feature["speechiness"] = request.form.get("speechiness")
+    feature["tempo"] = request.form.get("tempo")
+    feature["valence"] = request.form.get("valence")
+    print(request.form)
+    print(feature)
+    # hit = popular_hit(acousticness=acousticness, danceability=danceability, energy=energy, instrumentalness=instrumentalness, liveness=liveness, loudness=loudness, speechiness=speechiness, tempo=tempo, valence=valence)
+    # db.session.add(hit)
+    # db.session.commit()
+
+    return render_template("index.html", feature=feature)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- heroku_deployment/spotify_hits_app/static/js/plot_copy.js
+++ heroku_deployment/spotify_hits_app/static/js/plot_copy.js
@@ -1,7 +1,11 @@
 // Create the gauge chart.
-var gaugePlot = document.getElementById('gauge');
 
 var plotConfig = {responsive: true}
+
+function optionChanged(newSample) {
+    // Fetch new data each time a new sample is selected
+    create_gauge_plot(newSample);
+  
 
 function create_gauge_plot(score) {
     var gaugeData = [{
@@ -19,88 +23,54 @@ function create_gauge_plot(score) {
             },
             bar: { color: "black" },
             steps: [
-                { range: [1, 2], color: "red" },
-                { range: [0, 1], color: "blue" }
+                { range: [0, 1], color: "red" },
+                { range: [1, 2], color: "blue" }
             ]
         }
     }];
 
     // Create the layout for the gauge chart.
     var gaugeLayout = {
-        autosize: true,
-        annotations: [{
-            xref: 'paper',
-            yref: 'paper',
-            x: 0.5,
-            xanchor: 'center',
-            y: -0.2,
-            yanchor: 'center',
-            text: "The gauge displays whether the track is popular or not",
-            showarrow: false
-        }]
-    };
-    Plotly.newPlot("gauge", gaugeData, gaugeLayout,plotConfig);
+        margin: { t: 100, b: 20 }
+      };
+    Plotly.newPlot("gauge", gaugeData, gaugeLayout, plotConfig);
 
-//     // Create the feature importances chart 
-//     var barData = [{
-//         x: ['Acousticness', 'Danceability', 'Energy', 'Instrumentalness', 'Liveness', 'Loudness', 'Speechiness', 'Tempo', 'Valence', 'Hit'],
-//         y: [],
-//         type: 'bar'
-//     }];
-//     var barLayout = {
-//         title: 'Most Important Features',
-//         font: {
-//             family: 'Times, sans-serif'
-//         },
-//         showlegend: false,
-//         xaxis: {
-//             tickangle: -45
-//         },
-//         yaxis: {
-//             title: 'Importance Score',
-//             zeroline: false,
-//             gridwidth: 2,
-//             tickformat: '.2f'
-//         },
-//         bargap: 0.05
-//     };
-
-//     Plotly.newPlot('barPlot', barData, barLayout, { responsive: true });
-
-// }
-
+}}
 // Function to display inputs after page has reloaded
-function repopulate(display_input) {
-    console.log(re_input);
-    // Check if inputs were entered
-    if (!(Object.keys(display_input).length === 0 && display_input.constructor === Object)) {
-        document.getElementById("inputAcousticness").value = display_input.selectAcousticness;
-        document.getElementById("inputDanceability").value = display_input.selectDanceability;
-        document.getElementById("inputEnergy").value = display_input.selectEnergy;
-        document.getElementById("inputInstrumentalness").value = display_input.selectInstrumentalness;
-        document.getElementById("inputLiveness").value = display_input.selectLiveness;
-        document.getElementById("inputLoudness").value = display_input.selectLoudness;
-        document.getElementById("inputSpeechiness").value = display_input.selectSpeechiness;
-        document.getElementById("inputTempo").value = display_input.selectTempo;
-        document.getElementById("inputValence").value = display_input.selectValence;
-        document.getElementById(display_input.isHit).checked = true;
-    }
-};
 
-document.getElementById("resetButton").onclick = function() { reset() };
 
-function reset() {
-    let zero = 0;
-    create_gauge_plot(zero);
-    document.getElementById("inputAcousticness").value = null;
-    document.getElementById("inputDanceability").value = null;
-    document.getElementById("inputEnergy").value = null;
-    document.getElementById("inputInstrumentalness").value = null;
-    document.getElementById("inputLiveness").value = null;
-    document.getElementById("inputLoudness").value = null;
-    document.getElementById("inputSpeechiness").value = null;
-    document.getElementById("inputTempo").value = null;
-    document.getElementById("inputValence").value = null;
-    let hits = document.getElementsByName("isHit");
-    hits.forEach((hit) => { hit.checked = false; });
-};}
+// function repopulate(display_input) {
+//     console.log(re_input);
+//     // Check if inputs were entered
+//     if (!(Object.keys(display_input).length === 0 && display_input.constructor === Object)) {
+//         document.getElementById("inputAcousticness").value = display_input.selectAcousticness;
+//         document.getElementById("inputDanceability").value = display_input.selectDanceability;
+//         document.getElementById("inputEnergy").value = display_input.selectEnergy;
+//         document.getElementById("inputInstrumentalness").value = display_input.selectInstrumentalness;
+//         document.getElementById("inputLiveness").value = display_input.selectLiveness;
+//         document.getElementById("inputLoudness").value = display_input.selectLoudness;
+//         document.getElementById("inputSpeechiness").value = display_input.selectSpeechiness;
+//         document.getElementById("inputTempo").value = display_input.selectTempo;
+//         document.getElementById("inputValence").value = display_input.selectValence;
+//         document.getElementById(display_input.isHit).checked = true;
+//     }
+// };
+
+// document.getElementById("resetButton").onclick = function() { reset() };
+
+// function reset() {
+//     let zero = 0;
+//     create_gauge_plot(zero);
+//     document.getElementById("inputAcousticness").value = null;
+//     document.getElementById("inputDanceability").value = null;
+//     document.getElementById("inputEnergy").value = null;
+//     document.getElementById("inputInstrumentalness").value = null;
+//     document.getElementById("inputLiveness").value = null;
+//     document.getElementById("inputLoudness").value = null;
+//     document.getElementById("inputSpeechiness").value = null;
+//     document.getElementById("inputTempo").value = null;
+//     document.getElementById("inputValence").value = null;
+//     let hits = document.getElementsByName("isHit");
+//     hits.forEach((hit) => { hit.checked = false; });
+// };
+   
